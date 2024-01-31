d71789c5c6d59705b97476e4d6e179f7c8c6d9ac
AkioWm <cherif.kante16@gmail.com>
2024-01-31 17:08:50 +0100

    Refactor HelloWorld Class

---
+++ src/main/java/com/openclassroom/helloworld/model/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.openclassroom.helloworld.model;
+
+import org.springframework.boot.CommandLineRunner;
+
+
+
+public class HelloWorld{
+
+    public String value = "Hello World";
+
+    public String toString(){
+        return value;
+    }
+
+
+}
