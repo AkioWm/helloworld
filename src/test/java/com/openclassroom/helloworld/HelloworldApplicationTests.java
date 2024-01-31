b7cdd908bb9a2052c1aca40a74d09671eb5445dc
AkioWm <cherif.kante16@gmail.com>
2024-01-31 18:02:50 +0100

    Create Unit Test

--- src/test/java/com/openclassroom/helloworld/HelloworldApplicationTests.java
+++ src/test/java/com/openclassroom/helloworld/HelloworldApplicationTests.java
@@ -1,13 +1,23 @@
 package com.openclassroom.helloworld;
 
+import com.openclassroom.helloworld.service.BusinessService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class HelloworldApplicationTests {
-
+	@Autowired
+	private BusinessService bs;
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void testGetHelloWorld(){
+		assertEquals(bs.getHelloWorld().getValue(),"Hello World");
 	}
 
 }
